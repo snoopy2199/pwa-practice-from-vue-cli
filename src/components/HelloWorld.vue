242056c6fdedbbce2ff886cba36bf0398b034074
snoopy2199 <snoopy2199@gmail.com>
2019-08-05 23:44:08 +0800

    Add fate function

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="hello">
-    <button @click="handleShare">share</button>
-    <input v-if="isCopyBoxShowing" type="text" value="Hello World!" />
+    {{fate}}
+    <button @click="chance">抽籤</button>
+    <button v-if="fate" @click="handleShare">分享</button>
+    <textarea v-if="isCopyBoxShowing" :value="message" />
   </div>
 </template>
 
@@ -10,14 +12,27 @@ export default {
   name: 'HelloWorld',
   data() {
     return {
+      fateList: [
+        '大吉', '中吉', '小吉', '吉', '末吉', '凶', '大凶',
+      ],
       isCopyBoxShowing: false,
+      fate: '',
     };
   },
+  computed: {
+    message() {
+      return `我在「PWA (Vue)」(https://snoopy2199.github.io/pwa-practice-from-vue-cli/)抽到了「${this.fate}」`;
+    },
+  },
   methods: {
+    chance() {
+      this.isCopyBoxShowing = false;
+      this.fate = this.fateList[Math.floor(Math.random() * this.fateList.length)];
+    },
     handleShare() {
       if (navigator.share) {
         navigator.share({
-          text: 'Hello World!',
+          text: this.message,
         });
       } else {
         this.isCopyBoxShowing = true;
