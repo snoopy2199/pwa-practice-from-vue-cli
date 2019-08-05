f7a52cd914618cb3226d903218055d980d4849af
snoopy2199 <snoopy2199@gmail.com>
2019-08-05 23:12:48 +0800

    Clean

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld />
     <button v-if="promptEvent" @click="handlePromotion">install</button>
   </div>
 </template>
@@ -52,7 +51,5 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
