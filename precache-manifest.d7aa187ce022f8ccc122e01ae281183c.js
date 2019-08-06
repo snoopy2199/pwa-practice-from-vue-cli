5541e640cea8d6c88e33189b6170c6c9a9e62677
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-06 00:41:27 +0000

    Deploy snoopy2199/pwa-practice-from-vue-cli to github.com/snoopy2199/pwa-practice-from-vue-cli.git:gh-pages

---
+++ precache-manifest.d7aa187ce022f8ccc122e01ae281183c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "ffc865825bf79abe732b",
+    "url": "js/chunk-vendors.7d86b3cf.js"
+  },
+  {
+    "revision": "c0831ebf1f0dfa303e13",
+    "url": "js/app.d3fe6f38.js"
+  },
+  {
+    "revision": "0aa8c3ba7c4ec8d8b022c65d088cecf7",
+    "url": "index.html"
+  },
+  {
+    "revision": "c0831ebf1f0dfa303e13",
+    "url": "css/app.867584c3.css"
+  }
+];
